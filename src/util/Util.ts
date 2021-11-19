4a3673994e9a513499c96b6dd7859ecc713613ca
yanuu1337 <yanuu.why@gmail.com>
2021-11-19 21:29:32 +0100

    feat(Util): publish slash commands

---
+++ src/util/Util.ts
@@ -0,0 +1,30 @@
+import DiscordClient from "../client/DiscordClient"
+
+export default class Util extends null {
+    constructor() {
+        throw new Error(`The class ${this.constructor.name} is not supposed to be instantiated!`)
+    }   
+
+    static publishCommands = async (client: DiscordClient) => {        
+        const body = client.commands.map(k => k.getData())
+        const response = await client.restAPI.put(`/applications/${client?.application?.id}/guilds/${client.config.id}/commands`, { body })
+
+        const slashCommands = client.commands.map((cmd) => {
+            if(!cmd.getPerms()) return;
+            //@ts-ignore
+            const apiCommand = response.find((k) => k.name === cmd.getName())
+
+            return {
+                id: apiCommand.id,
+                permissions: cmd.getPerms()
+            }
+        }).map((el) => el)
+        if(!slashCommands || slashCommands.length === 0) return;
+        //@ts-ignore
+        
+        const res = await client.restAPI.put(`/applications/${client?.application?.id}/guilds/${client.config.id}/commands/permissions`, { body: slashCommands})
+        console.log(res)
+    }
+
+    
+}
