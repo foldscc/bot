ec07a077a0ecdf01e7171fa1d46d0b0cd6fef7d3
yanuu1337 <yanuu.why@gmail.com>
2021-11-21 21:39:27 +0100

    types: Command run fn param types

--- src/structure/Command.ts
+++ src/structure/Command.ts
@@ -2,17 +2,18 @@ import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "@discor
 import { AdminRolePermissionData, ModRolePermissionData, RolePermission } from "../util/Constants";
 import client from "..";
 import DiscordClient from "../client/DiscordClient";
-import { CommandInteraction, CommandInteractionOptionResolver, Guild } from "discord.js";
+import { CacheType, CommandInteraction, CommandInteractionOptionResolver, Guild } from "discord.js";
 import { Model } from "sequelize/types";
 export default abstract class Command {
     abstract commandName: string;
     abstract commandDescription: string;
+    dmAllowed?: false;
     data?: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder; 
     devOnly?: boolean;
     perms: RolePermission = 'USER';
 
 
-    async run(client: DiscordClient, interaction: CommandInteraction, options: CommandInteractionOptionResolver, guild: Guild, userProfile?: Model<any, any>, userLanguage?: any) {}
+    async run(client: DiscordClient, interaction: CommandInteraction, options: Omit<CommandInteractionOptionResolver<CacheType>, "getMessage" | "getFocused">, guild: Guild, userProfile?: Model<any, any>, userLanguage?: any) {}
 
     
     getData() {
