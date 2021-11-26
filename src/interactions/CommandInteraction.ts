2df1db6a56d6e47a043ca669770864d14dc1c369
yanuu1337 <yanuu.why@gmail.com>
2021-11-26 22:23:04 +0100

    feat: prevent any command errors

--- src/interactions/CommandInteraction.ts
+++ src/interactions/CommandInteraction.ts
@@ -12,18 +12,27 @@ export default class CommandInteractionHandler extends HandledInteraction {
         const command = client.commands.get(interaction.commandName);
         if(!command) return;
         if((!interaction.channel || interaction?.channel.type === "DM") && !command.dmAllowed) {
-            interaction.reply({embeds: [EmbedFactory.generateErrorEmbed(`Używanie komend`, `Komenda \`${command.getName()}\` nie może być używana w wiadomościach prywatnych!`)]})
+            interaction.reply({embeds: [EmbedFactory.generateErrorEmbed(`Użycie komendy`, `Komenda \`${command.getName()}\` nie może być używana w wiadomościach prywatnych!`)]})
             return;
         }
         if(command?.devOnly && interaction.user.id !== "304263386588250112") {
-            interaction.reply({embeds: [EmbedFactory.generateErrorEmbed(`Używanie komend`, `Ta komenda może zostać użyta tylko przez dewelopera!`)]})
+            interaction.reply({embeds: [EmbedFactory.generateErrorEmbed(`Użycie komendy`, `Komenda \`${command.getName()}\` może zostać użyta tylko przez dewelopera!`)]})
             return;
         }
         const guild = client.guilds.cache.get(client.config.id) || await client.guilds.fetch(client.config.id)
         const interactionUserProfile = await Player.findOne({where: {discordId: interaction.user.id}})
         
+        try {
 
-        command.run(client, interaction, interaction.options, guild, interactionUserProfile)
+            command.run(client, interaction, interaction.options, guild, interactionUserProfile)
+        } catch (err) {
+            client.emit('adminWarn', `Wystąpił błąd: ${err.name}`, err)
+            if(interaction.replied) {
+                interaction.editReply({embeds: [EmbedFactory.generateErrorEmbed(`Użycie komendy`, `Wystąpił nieznany błąd!`)]})
+                return;
+            }
+            interaction.reply({embeds: [EmbedFactory.generateErrorEmbed(`Użycie komendy`, `Wystąpił nieznany błąd!`)]})
+        }
 
     }
 }
